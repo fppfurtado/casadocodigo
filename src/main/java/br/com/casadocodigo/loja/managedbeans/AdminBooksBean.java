b90b4841c2c460c87d2c4a3aeb3063a1484d22c9
Fernando Furtado <fppfurtado@gmail.com>
2020-03-03 23:13:53 -0300

    Binding form.xhtml to class Book

--- src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
@@ -2,11 +2,19 @@ package br.com.casadocodigo.loja.managedbeans;
 
 import javax.enterprise.inject.Model;
 
+import br.com.casadocodigo.loja.models.Book;
+
 @Model
 public class AdminBooksBean {
 	
+	private Book book = new Book();
+	
+	public Book getBook() {
+		return book;
+	}
+
 	public void save( ) {
-		System.out.println("salvando livro...");
+		System.out.println("salvando o livro:\n" + book);
 	}
 
 }
